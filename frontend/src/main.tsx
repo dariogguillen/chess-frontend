6d6c031e8c5e4c326b8d126b66113c619b0d3ef2
dariogguillen <guillendario@gmail.com>
2024-10-23 15:50:35 -0600

    feat: home route added

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -6,8 +6,11 @@ import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
+import { HomePage } from "./pages/home";
+
 const router = createBrowserRouter([
   { path: "/", element: <App /> },
+  { path: "/home", element: <HomePage /> },
 ]);
 
 createRoot(document.getElementById("root")!).render(
---
+++ frontend/src/pages/home/home.tsx
@@ -0,0 +1,7 @@
+import Header from "../../components/Header/Header";
+
+const Home = () => {
+  return <Header authed={true} />;
+};
+
+export default Home;
